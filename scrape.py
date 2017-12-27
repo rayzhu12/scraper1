8e5b20b9c07d98541dd7b9818d8a2b87ab7c0949
Raymond <rayzhu12@gmail.com>
2017-12-26 23:59:35 -0500

    saved to CSV

--- scrape.py
+++ scrape.py
@@ -1,3 +1,4 @@
+import csv
 import requests
 from BeautifulSoup import BeautifulSoup
 
@@ -8,6 +9,15 @@ html = response.content
 soup = BeautifulSoup(html)
 table = soup.find('tbody', attrs={'class': 'stripe'})
 
+list_of_rows = []
 for row in table.findAll('tr'):
+    list_of_cells = []
     for cell in row.findAll('td'):
-        print cell.text.replace('&nbsp;', '')
+        text = cell.text.replace('&nbsp;', '')
+        list_of_cells.append(text)
+    list_of_rows.append(list_of_cells)
+
+outfile = open("./inmates.csv", "wb")
+writer = csv.writer(outfile)
+writer.writerow(["Last", "First", "Middle", "Gender", "Race", "Age", "City", "State"])
+writer.writerows(list_of_rows)
